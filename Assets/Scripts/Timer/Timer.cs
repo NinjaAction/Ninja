ce7dceee95e96347122f0612c7621a1f42c40a9a
C03940926 <C0394@oic.jp>
2022-11-13 08:12:44 +0900

    1-3 Timer
    タイマーの設定と表示

---
+++ Assets/Scripts/Timer/Timer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    private float countTime = 0;
+    private int SetTimer;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // countTimeに、ゲームが開始してからの秒数を格納
+        countTime += Time.deltaTime;
+
+        SetTimer = 180 - (int)countTime;
+
+        // 小数2桁にして表示
+        GetComponent<Text>().text = SetTimer.ToString("F2");
+    }
+
+}
